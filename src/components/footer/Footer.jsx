03c959506b663aa095b1b7c652668bd484d6a6f1
CWells1225 <ebecochuba@yahoo.com>
2023-09-21 00:44:28 -0400

    media query changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,6 @@
 import "./footer.css";
+import LinkedIn from "../../asset/images/linkedin_icon.png";
+import Instagram from "../../asset/images/instagram_icon.webp";
 
 const Footer = () => {
   return (
@@ -9,19 +11,19 @@ const Footer = () => {
         <div className="footer-row1">
         <div className="contact">
         <div className="email">
-        <span className="text-span-2">want to collaborate?</span> &nbsp;&nbsp;
-        <a href="mailto:team@pearllumi.com" className="mailto">
-        <span className="text-span-3">TEAM@PEARLLUMI.CO</span>
-        </a>
+          <span className="text-span-2">want to collaborate?</span> &nbsp;&nbsp;
+          <a href="mailto:team@pearllumi.com" className="mailto">
+          <span className="text-span-3">TEAM@PEARLLUMI.CO</span>
+          </a>
         </div>
         </div>
         <div className="social">
-        <a href="https://www.instagram.com/pearllumi.creative/" className="instagram w-inline-block">
-        <img src="https://uploads-ssl.webflow.com/5fb80387d0b0b18ea3a62288/5fbd25f4eb20a874daa80d25_instagram.svg" loading="lazy" alt="" />
-        </a>
-        <a href="https://www.linkedin.com/company/pearllumi" className="linkedin w-inline-block">
-        <img src="https://uploads-ssl.webflow.com/5fb80387d0b0b18ea3a62288/5fbd25f4f6893e45b40800c6_linkedin.svg" loading="lazy" alt="" />
-        </a>
+          <a href="https://www.instagram.com/pearllumi.creative/" className="instagram w-inline-block">
+            <div className="item"><img src={Instagram} alt="instagram logo" width= "20px" height="20px" /> </div>
+          </a>
+          <a href="https://www.linkedin.com/company/pearllumi" className="linkedin w-inline-block">
+            <div className="item"><img src={LinkedIn} alt="linkedin logo" width= "18px" height="18px" /> </div>
+          </a>
         </div>
         </div>
         <div className="footer-row2">
@@ -34,3 +36,5 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
